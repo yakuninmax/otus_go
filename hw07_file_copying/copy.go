4ae48d29c25f81831c82ff53862580cc356e8aae
Max Yakunin <yakunin.max@gmail.com>
2024-06-06 22:24:16 +0300

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,85 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrUnsupportedFile         = errors.New("unsupported file")
+	ErrOffsetExceedsFileSize   = errors.New("offset exceeds file size")
+	ErrFileNotFound            = errors.New("source file not found")
+	ErrOpenSourceFile          = errors.New("could not open source file")
+	ErrGetOffset               = errors.New("could not get offset for source file")
+	ErrCreatingDestinationFile = errors.New("creating destination file failed")
+	ErrCopyFile                = errors.New("could not copy file")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	// Get file info and size.
+	sourceFileInfo, err := os.Stat(fromPath)
+	// Check if file exists.
+	if err != nil {
+		return ErrFileNotFound
+	}
+
+	// Check if regular file.
+	if !sourceFileInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	// Check if offset greater file len.
+	sourceFileSize := sourceFileInfo.Size()
+
+	if offset > sourceFileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	// Create destination file.
+	destinationFile, err := os.Create(toPath)
+	if err != nil {
+		return ErrCreatingDestinationFile
+	}
+	defer destinationFile.Close()
+
+	// Open source file.
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return ErrOpenSourceFile
+	}
+
+	// Set starting position using offset.
+	_, err = sourceFile.Seek(offset, 0)
+	if err != nil {
+		return ErrGetOffset
+	}
+
+	// Get bytes count to copy except offset.
+	bytesCount := sourceFileSize - offset
+	if limit != 0 && limit < bytesCount {
+		bytesCount = limit
+	}
+
+	// Create progress bar.
+	bar := pb.New(int(bytesCount)).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 10)
+	bar.ShowSpeed = true
+	bar.Start()
+
+	// Create proxy reader.
+	reader := bar.NewProxyReader(sourceFile)
+
+	// Copy data from source to destination file.
+	_, err = io.CopyN(destinationFile, reader, bytesCount)
+	if err != nil {
+		return ErrCopyFile
+	}
+	defer sourceFile.Close()
+
+	// Finish progress bar.
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,140 @@
 package main
 
-import "testing"
+import (
+	"crypto/md5"
+	"io"
+	"log"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	// Reference file name.
+	inputFile := path.Join("testdata", "input.txt")
+
+	// Test case structure.
+	type testCase struct {
+		name          string
+		inputFile     string
+		referenceFile string
+		offset        int64
+		limit         int64
+		expectedError error
+	}
+
+	// Tests list.
+	tests := []testCase{
+		{
+			name:          "offset 0 limit 0",
+			inputFile:     inputFile,
+			referenceFile: path.Join("testdata", "out_offset0_limit0.txt"),
+			offset:        0,
+			limit:         0,
+		},
+		{
+			name:          "offset 0 limit 10",
+			inputFile:     inputFile,
+			referenceFile: path.Join("testdata", "out_offset0_limit10.txt"),
+			offset:        0,
+			limit:         10,
+		},
+		{
+			name:          "offset 0 limit 1000",
+			inputFile:     inputFile,
+			referenceFile: path.Join("testdata", "out_offset0_limit1000.txt"),
+			offset:        0,
+			limit:         1000,
+		},
+		{
+			name:          "offset 0 limit 10000",
+			inputFile:     inputFile,
+			referenceFile: path.Join("testdata", "out_offset0_limit10000.txt"),
+			offset:        0,
+			limit:         10000,
+		},
+		{
+			name:          "offset 100 limit 1000",
+			inputFile:     inputFile,
+			referenceFile: path.Join("testdata", "out_offset100_limit1000.txt"),
+			offset:        100,
+			limit:         1000,
+		},
+		{
+			name:          "offset 6000 limit 1000",
+			inputFile:     inputFile,
+			referenceFile: path.Join("testdata", "out_offset6000_limit1000.txt"),
+			offset:        6000,
+			limit:         1000,
+		},
+		{
+			name:          "irregular source file",
+			inputFile:     "/dev/urandom",
+			expectedError: ErrUnsupportedFile,
+		},
+		{
+			name:          "offset exceeds file size",
+			inputFile:     inputFile,
+			offset:        100000,
+			expectedError: ErrOffsetExceedsFileSize,
+		},
+		{
+			name:          "source file not found",
+			inputFile:     inputFile + "hhgjh",
+			expectedError: ErrFileNotFound,
+		},
+	}
+
+	// Run tests.
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Create temp file in temp dir.
+			tempFile, err := os.CreateTemp("", "ddtest*")
+			if err != nil {
+				log.Println(err)
+			}
+			// Cleanup.
+			defer os.Remove(tempFile.Name())
+
+			// Run copy function.
+			err = Copy(test.inputFile, tempFile.Name(), test.offset, test.limit)
+
+			// Check results.
+			if err != nil {
+				// Check for errors.
+				require.EqualError(t, err, test.expectedError.Error())
+			} else {
+				// Compare reference & temp copied file.
+				assert.Equal(t, md5checksum(t, test.referenceFile), md5checksum(t, tempFile.Name()))
+			}
+		})
+	}
+}
+
+// Calculate MD5 checksum.
+func md5checksum(t *testing.T, filename string) []byte {
+	// Define as testing helper.
+	t.Helper()
+
+	// Open file.
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+
+	// Create md5 hash.
+	md5Hash := md5.New()
+
+	// Get file md5 hash.
+	_, err = io.Copy(md5Hash, file)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Return md5 checksum.
+	return md5Hash.Sum(nil)
 }
