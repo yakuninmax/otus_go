b97f59b879dcb1bfe18fa09342e9b0ccdd1a1319
Max Yakunin <msyakuni@mts.ru>
2024-06-24 17:17:21 +0300

    HW06 completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,34 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := wrap(in, done)
+
+	for _, stage := range stages {
+		out = stage(wrap(out, done))
+	}
+
+	return out
+}
+
+// Wrap channels
+func wrap(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+
+	return out
 }
