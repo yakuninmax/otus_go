7dfed6242da99388bbd46d58f81ec1dd818844a1
Max Yakunin <msyakuni@mts.ru>
2024-07-19 23:50:16 +0300

    Fix tests

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -103,7 +103,6 @@ func TestValidate(t *testing.T) {
 		expextedErrors := &tt.expectedErr
 		structs := &tt.in
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			t.Parallel()
 			validationErrors := Validate(structs)
 			require.ErrorAs(t, validationErrors, expextedErrors)
 		})
