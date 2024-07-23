cb3e8c1a4df87ac12aa5c95f971509ebf6fdfa90
Max Yakunin <msyakuni@mts.ru>
2024-07-23 21:01:54 +0300

    Remove test tag

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -15,7 +15,7 @@ type (
 	User struct {
 		ID     string `json:"id" validate:"len:36"`
 		Name   string
-		Age    int             `validate:"min:ss|max:50"`
+		Age    int             `validate:"min:18|max:50"`
 		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
 		Role   UserRole        `validate:"in:admin,stuff"`
 		Phones []string        `validate:"len:11"`
